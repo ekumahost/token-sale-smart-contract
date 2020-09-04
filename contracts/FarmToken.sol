49c09fa1649e4ad2a61f4b7289d39afbc51b0e5b
Okokoh Benjamin <ifihear@gmail.com>
2020-09-04 08:14:49 +0100

    complete ERC20 Token creation

--- contracts/FarmToken.sol
+++ contracts/FarmToken.sol
@@ -1,38 +1,33 @@
 pragma solidity >=0.4.21 <0.7.0;
 // pragma solidity ^0.5.16; // our local truffle compiler version: is 0.5.16+commit.9c3226ce.Emscripten.clang
 
-
 contract FarmToken{
 
-// Constructor
-// set total token
-// get total token
-// asign name
-string public name = "FarmToken"; // getter for name
-// asign symbol
-string public symbol = "FARM"; 
-string public standard = "FarmToken v1.0"; // we just added it, not part of ERC20 
+	// Constructor
+	// set total token
+	// get total token
+	// asign name
+	string public name = "FarmToken"; // getter for name
+	// asign symbol
+	string public symbol = "FARM"; 
+	string public standard = "FarmToken v1.0"; // we just added it, not part of ERC20 
 
 
+	event Transfer(
 
-
-event Transfer(
-
-address indexed _from, 
-address indexed _to,
-uint256 _value
+		address indexed _from, 
+		address indexed _to,
+		uint256 _value
 
 	);
 
 
-event Approval(
-    address indexed _owner,
-    address indexed _spender,
-    uint256 _value
+	event Approval(
+    	address indexed _owner,
+    	address indexed _spender,
+    	uint256 _value
 
 	);
-
-
 
 
      // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
@@ -49,27 +44,25 @@ event Approval(
 
      	// allocate the initial supply to a particular address 
 
-
    }
-
 
      // Transfer
 
-     function transfer(address _to, uint256 _value) public returns (bool success) {
- // exception if account dont have enough balance.
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+ 		// exception if account dont have enough balance.
        require(balanceOf[msg.sender] >= _value); // if condition is meet code continue.
 
 
-    // tranfer balance
-     balanceOf[msg.sender] -= _value;
-     balanceOf[_to] += _value;
+    	// tranfer balance
+     	balanceOf[msg.sender] -= _value;
+     	balanceOf[_to] += _value;
 
 
-     // return true or fale
-     // transfer event
-     emit Transfer(msg.sender, _to, _value );
+     	// return true or fale
+     	// transfer event
+     	emit Transfer(msg.sender, _to, _value );
 
-// tranfer function test not passing yet, sayiing no event was fired...
+		// tranfer function test not passing yet, sayiing no event was fired...
 
         return true;
 
@@ -77,67 +70,55 @@ event Approval(
 
 
 
-// approve
-     function approve(address _spender, uint256 _value) public returns (bool success) {
+	 // approve
+    function approve(address _spender, uint256 _value) public returns (bool success) {
 
 
 
            allowance[msg.sender][_spender] = _value;
 
-// emit Approval
-    emit Approval(msg.sender, _spender, _value );
-    return true;
+	      // emit Approval
+    	emit Approval(msg.sender, _spender, _value );
+    	return true;
 
 
-     	}
+     }
 
 
+	// Deletgated Transfer from one account to another without the sender initiating it..
 
 
+	   // tansfer from
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+
+		// user to user transfer
+
+		// require _from has enough token
+
+		require (_value <= balanceOf[_from]);
 
 
+		// require allowance is big enough 
+		require (_value <= allowance[_from][msg.sender]);
 
-// Deletgated Transfer from one account to another without the sender initiating it..
-
-
-// tansfer from
-
-function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
-
-// user to user transfer
-
-// require _from has enough token
-
-require (_value <= balanceOf[_from]);
+		// change balance
+     	balanceOf[_from] -= _value;
+     	balanceOf[_to] += _value;
 
 
-// require allowance is big enough 
-require (_value <= allowance[_from][msg.sender]);
-
-// change balance
-     balanceOf[_from] -= _value;
-     balanceOf[_to] += _value;
+		// update allowance
+		allowance[_from][msg.sender] -= _value;
 
 
-// update allowance
-allowance[_from][msg.sender] -= _value;
+		// transfer event
+		emit Transfer(_from, _to,_value);
 
+		// return true.
 
-// transfer event
-emit Transfer(_from, _to,_value);
+		return true;
 
-// return true.
-
-return true;
-
-}
-
-
-
-
-
-
-
+	}
 
 
 }
